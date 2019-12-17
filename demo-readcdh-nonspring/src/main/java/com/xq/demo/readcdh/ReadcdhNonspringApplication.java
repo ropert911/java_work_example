4b249a68afe6f7189f74c9cfc97b8b922af139b2
ropert911 <254323626@qq.com>
2019-12-17 15:37:33 +0800

    示例调整

---
+++ demo-readcdh-nonspring/src/main/java/com/xq/demo/readcdh/ReadcdhNonspringApplication.java
@@ -0,0 +1,13 @@
+package com.xq.demo.readcdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadcdhNonspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadcdhNonspringApplication.class, args);
+    }
+
+}
---
+++ demo-readcdh-nonspring/src/test/java/com/xq/demo/readcdh/ReadcdhNonspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xq.demo.readcdh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadcdhNonspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
