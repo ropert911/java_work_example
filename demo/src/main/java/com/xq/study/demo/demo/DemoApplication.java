1e32d267e9630a641eae0c19f0564053f7fbbb0b
ropert911 <ropert911@163.com>
2020-05-06 06:15:48 +0800

    添加示例说明

--- demo/src/main/java/com/xq/study/demo/demo/DemoApplication.java
+++ demo/src/main/java/com/xq/study/demo/demo/DemoApplication.java
@@ -1,29 +1,16 @@
 package com.xq.study.demo.demo;
 
-import com.vmware.ovsdb.exception.OvsdbClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
-public class DemoApplication implements ApplicationRunner {
+public class DemoApplication {
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws OvsdbClientException, IOException {
-        OvsdbClientPassiveConnectionTest test = new OvsdbClientPassiveConnectionTest();
-        test.setUp(false);
-        test.testTcpConnection();
-//        test.tearDown();
     }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+demo-dataforbigdata     模拟iot把处理后的数据放到 itc-origin-data中
+demo-ovsdb-controller
+    ovsdb测试程序，无特定目的
